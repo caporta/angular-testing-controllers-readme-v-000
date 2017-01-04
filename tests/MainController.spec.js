c80165ca10898f9632d5bf6d0967cfdd1f068ff0
Chris Aporta <christopher.aporta@gmail.com>
2017-01-03 22:42:31 -0500

    complete lab

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -1,5 +1,15 @@
-describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
-    });
-});
+describe('MainController', () => {
+  beforeEach(module('app'))
+
+  let $controller
+
+  beforeEach(inject(_$controller_ => {
+    $controller = _$controller_
+  }))
+
+  it('should have Steve Jobs\'s name', () => {
+    const controller = $controller('MainController', { $scope: {} })
+
+    expect(controller.name).toEqual('Steve Jobs')
+  })
+})
